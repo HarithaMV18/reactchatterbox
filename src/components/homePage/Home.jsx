3c2800030a8e0e7c0eb6b3316f630ad523546a5c
haritha <haritha.mvijaya@gmail.com>
2024-01-23 11:01:06 +0530

    first commit

--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./home.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import HomeLeftSideDetails from "./HomeLeftSideDetails";
 
 const Home = () => {
   return (
     <div className="home__component">
       <nav className="home__navbar">
-        <div className="logo">
-          <h1>ChatterBox</h1>
+        <div className="nav__left__header">
+          <div className="logo">
+            <h1>ChatterBox</h1>
+          </div>
+          <div className="profile-details">
+            <div className="profile-photo"></div>
+            <p>Haritha</p>
+            <button>Logout</button>
+          </div>
         </div>
       </nav>
       <HomeLeftSideDetails />
